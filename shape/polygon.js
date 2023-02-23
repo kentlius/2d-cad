89803e5e28919938e7d219888935265c59d18360
Saul Sayers <saulsayers@gmail.com>
2023-02-24 05:52:17 +0700

    Warna

--- shape/polygon.js
+++ shape/polygon.js
@@ -1,46 +1,53 @@
 export class Polygon {
   constructor() {
-    this.vertices = [];
+    this.data = [];
     this.acceptedRadius = 0.1;
   }
 
   addVertex(x, y, colors) {
-    this.vertices.push(x, y, colors[0], colors[1], colors[2], colors[3]);
+    this.data.push(x, y, colors[0], colors[1], colors[2], colors[3]);
   }
 
   updateVertex(x,y){
-    this.vertices[this.vertices.length - 6] = x;
-    this.vertices[this.vertices.length - 5] = y;
+    this.data[this.data.length - 6] = x;
+    this.data[this.data.length - 5] = y;
   }
 
   deleteLast(){
-    this.vertices.splice(this.vertices.length - 6, 6);  
+    this.data.splice(this.data.length - 6, 6);  
   }
 
   removeVertex(n){
-    this.vertices.splice(n, 6);
+    this.data.splice(n, 6);
   }
   
   touchVertex(x, y){
     console.log("lokasi",x,y)
-    for(let i = 0; i < this.vertices.length; i += 6){
-      if(Math.abs(this.vertices[i] - x) < this.acceptedRadius && Math.abs(this.vertices[i+1] - y) < this.acceptedRadius){
+    for(let i = 0; i < this.data.length; i += 6){
+      if(Math.abs(this.data[i] - x) < this.acceptedRadius && Math.abs(this.data[i+1] - y) < this.acceptedRadius){
         return i;
       }
     }
     return -1;
   }
 
+  updateColor(i, color){
+    this.data[i+2] = color[0];
+    this.data[i+3] = color[1];
+    this.data[i+4] = color[2];
+    this.data[i+5] = color[3];
+  }
+
   render(gl) {
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
-    if (this.vertices.length > 12) {
-      gl.drawArrays(gl.TRIANGLE_FAN, 0, this.vertices.length / 6);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.data), gl.STATIC_DRAW);
+    if (this.data.length > 12) {
+      gl.drawArrays(gl.TRIANGLE_FAN, 0, this.data.length / 6);
     }
-    else if (this.vertices.length == 12) {
-      gl.drawArrays(gl.LINES, 0, this.vertices.length / 6);
+    else if (this.data.length == 12) {
+      gl.drawArrays(gl.LINES, 0, this.data.length / 6);
     }
     else{
-      gl.drawArrays(gl.POINTS, 0, this.vertices.length / 6);
+      gl.drawArrays(gl.POINTS, 0, this.data.length / 6);
     }
   }
 }
