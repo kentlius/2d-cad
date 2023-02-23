4a45bf195d7128baaf1d067a18224eb3126043f7
Saul Sayers <saulsayers@gmail.com>
2023-02-23 18:35:34 +0700

    fix poligon

--- shape/polygon.js
+++ shape/polygon.js
@@ -8,10 +8,16 @@ export class Polygon {
         this.vertices.push(x, y, colors[0], colors[1], colors[2], colors[3]);
     }
 
-  
     render(gl) {
-      console.log(this.vertices)
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
-      gl.drawArrays(gl.TRIANGLE_FAN, 0, 6);
+      if (this.vertices.length > 12) {
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, this.vertices.length / 6);
+      }
+      else if (this.vertices.length == 12) {
+        gl.drawArrays(gl.LINES, 0, this.vertices.length / 6);
+      }
+      else{
+        gl.drawArrays(gl.POINTS, 0, this.vertices.length / 6);
+      }
     }
   }
