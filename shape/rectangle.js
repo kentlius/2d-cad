3bb1cbac9b3fb879e811f3bb4d1fd2e71254b94b
kentliu <kent.delatinos@gmail.com>
2023-02-23 15:19:53 +0700

    feat: add rectangle & square

--- shape/rectangle.js
+++ shape/rectangle.js
@@ -1,38 +1,23 @@
 export class Rectangle {
-  constructor(x, y, width, height, color) {
+  constructor(x, y, width, height, colors) {
     this.x1 = x;
     this.x2 = x + width;
     this.y1 = y;
     this.y2 = y + height;
 
-    this.vertices = [
-      this.x1, this.y1, // top left
-      this.x2, this.y1, // top right
-      this.x1, this.y2, // bottom left
-      this.x2, this.y1, // top right
-      this.x1, this.y2, // bottom left
-      this.x2, this.y2 // bottom right
+    // prettier-ignore
+    this.data = [
+      this.x1, this.y1, colors[0], colors[1], colors[2], colors[3], // top left
+      this.x2, this.y1, colors[4], colors[5], colors[6], colors[7], /// top right
+      this.x1, this.y2, colors[8], colors[9], colors[10], colors[11], /// bottom left
+      this.x2, this.y1, colors[4], colors[5], colors[6], colors[7], /// top right
+      this.x1, this.y2, colors[8], colors[9], colors[10], colors[11], /// bottom left
+      this.x2, this.y2, colors[12], colors[13], colors[14], colors[15], /// bottom right
     ]
-
-    this.color = [
-      Math.random(), Math.random(), Math.random(), 1,
-      0.9, 0.7, 0.1, 1,
-      0.1, 0.3, 0.5, 1,
-      0.9, 0.7, 0.1, 1,
-      0.1, 0.3, 0.5, 1,
-      Math.random(), Math.random(), Math.random(), 1
-    ];
   }
 
   render(gl) {
-    const rectBuffer = initBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(this.vertices));
-    gl.bindBuffer(gl.ARRAY_BUFFER, rectBuffer);
-    initAttribute(gl, rectBuffer, 2);
-
-    const colorBuffer = initBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(this.color));
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    initAttribute(gl, colorBuffer, 4);
-
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.data), gl.STATIC_DRAW);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
   }
 }
---
+++ shape/square.js
@@ -0,0 +1,23 @@
+export class Square {
+    constructor(x, y, size, colors) {
+      this.x1 = x;
+      this.x2 = x + size;
+      this.y1 = y;
+      this.y2 = y + size;
+  
+      // prettier-ignore
+      this.data = [
+        this.x1, this.y1, colors[0], colors[1], colors[2], colors[3], // top left
+        this.x2, this.y1, colors[4], colors[5], colors[6], colors[7], /// top right
+        this.x1, this.y2, colors[8], colors[9], colors[10], colors[11], /// bottom left
+        this.x2, this.y1, colors[4], colors[5], colors[6], colors[7], /// top right
+        this.x1, this.y2, colors[8], colors[9], colors[10], colors[11], /// bottom left
+        this.x2, this.y2, colors[12], colors[13], colors[14], colors[15], /// bottom right
+      ]
+    }
+  
+    render(gl) {
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.data), gl.STATIC_DRAW);
+      gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+  }
